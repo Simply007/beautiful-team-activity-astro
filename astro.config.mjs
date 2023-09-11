4a65389260a27cfe5b7a74c5f0832daa452111ab
Ondřej Chrastina <ondrejch@kontent.ai>
2023-09-11 14:53:58 +0200

    test out version 0.0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import kontentAiAstro from "@simply007org/kontent-ai-astro";
+import contentful from "contentful-astro";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    kontentAiAstro({
+      deliveryClientConfig: {
+        environmentId: "f9397322-2816-00bd-c3d1-8686576112ae",
+      },
+      components: {
+        // add your components here
+        test: "components/Test",
+      },
+    }),
+    // contentful({
+    //   space: "iz8i3gwvhfzc",
+    //   accessToken:
+    //     "676373b2825f233d66488d4354f264535275faebcdee4bfc45cf15c68ee472e4",
+    //   components: {
+    //     test: "components/Test",
+    //   },
+    // }),
+  ],
 });
--- src/lib/kontent-ai.ts
+++ src/lib/kontent-ai.ts
@@ -2,7 +2,7 @@ import { createDeliveryClient } from "@kontent-ai/delivery-sdk";
 import packageInfo from "../../package.json";
 
 export const client = createDeliveryClient({
-    projectId: "f9397322-2816-00bd-c3d1-8686576112ae",
+    environmentId: "f9397322-2816-00bd-c3d1-8686576112ae",
     globalHeaders: (config) => (config.customHeaders || []).concat([{
         header: "X-KC-SOURCE",
         value: `${packageInfo.name};${packageInfo.version}`
